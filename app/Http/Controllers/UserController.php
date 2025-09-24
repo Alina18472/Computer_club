62a17669523afae766c759497ea53b68efde71db
TwiLightDM <dmitriy011004@gmail.com>
2025-09-24 18:33:57 +0400

    + Измены миграции + Добавлен Makefile для удобства + Добавлены crud к сущностям tariff, payment, booking, computerSpec, computerPosition, computer, code, additionalMenu, food, user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+
 class UserController extends Controller
 {
     public function index()
     {
+        return User::all();
+    }
 
+    public function store(StoreUserRequest $request)
+    {
+        return User::create($request->all());
+    }
+
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return $user;
+    }
+
+    public function destroy(User $user)
+    {
+        return $user->delete();
     }
 }
